446ca12670f5e3ecd137fd2adf7aba548fe2cff5
MrRikkert <hendrikx.rik@gmail.com>
2020-06-11 08:55:15 +0200

    use proper photo_url

--- src/app/components/tweet/tweet.component.ts
+++ src/app/components/tweet/tweet.component.ts
@@ -10,6 +10,8 @@ import { Tweet } from "src/app/models/tweet";
 export class TweetComponent implements OnInit {
   @Input()
   tweet: Tweet;
+  defaultPhotoURL =
+    "https://firebasestorage.googleapis.com/v0/b/kwetter-flutter.appspot.com/o/assets%2Fdefault-user-icon.png?alt=media&token=eecaee30-967d-4075-9840-2e89c18f3a84";
 
   constructor(private storage: AngularFireStorage) {
     this.storage
